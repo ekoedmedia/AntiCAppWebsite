43784a1c493d1f8935580a5f66ceea1016008012
ekoedmedia <ekoedmedia@gmail.com>
2014-02-14 03:12:37 -0700

    Added Interactions CSS and Views, updated controller

--- src/AntiC/Console/Controller/InteractionsController.php
+++ src/AntiC/Console/Controller/InteractionsController.php
@@ -12,4 +12,17 @@ class InteractionsController
         return $app['twig']->render('interactions/index.html.twig');
     }
 
+    public function addAction(Application $app)
+    {
+        return $app['twig']->render('interactions/add.html.twig');
+    }
+
+    public function editAction(Request $request, Application $app)
+    {
+        // Query Database with ID and Return Interactions Name and Information to Twig
+        return $app['twig']->render('interactions/edit.html.twig', array(
+                'interaction_name' => $request->get('ID')
+            ));
+    }
+
 }
