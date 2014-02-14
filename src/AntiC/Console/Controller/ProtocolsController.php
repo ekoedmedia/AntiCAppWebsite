735e2cc3fdf56cde1c82cbbc3a4b13a7b1be95f2
ekoedmedia <ekoedmedia@gmail.com>
2014-02-14 03:13:38 -0700

    Added Add/Edit View and CSS for Protocols, updated Controller

--- src/AntiC/Console/Controller/ProtocolsController.php
+++ src/AntiC/Console/Controller/ProtocolsController.php
@@ -12,4 +12,17 @@ class ProtocolsController
         return $app['twig']->render('protocols/index.html.twig');
     }
 
+    public function addAction(Application $app)
+    {
+        return $app['twig']->render('protocols/add.html.twig');
+    }
+
+    public function editAction(Request $request, Application $app)
+    {
+        // Query Database with ID and Return Protocol Name and Information to Twig
+        return $app['twig']->render('protocols/edit.html.twig', array(
+                'protocol_name' => $request->get('ID')
+            ));
+    }
+
 }
