6ea642bec510d16bbe94b040e41ab959c6da034d
ekoedmedia <ekoedmedia@gmail.com>
2014-02-14 03:11:44 -0700

    Added Drug Add/Edit Views

--- src/AntiC/Console/Controller/DrugsController.php
+++ src/AntiC/Console/Controller/DrugsController.php
@@ -12,4 +12,17 @@ class DrugsController
         return $app['twig']->render('drugs/index.html.twig');
     }
 
+    public function addAction(Application $app)
+    {
+        return $app['twig']->render('drugs/add.html.twig');
+    }
+
+    public function editAction(Request $request, Application $app)
+    {
+        // Query Database with ID and Return Drug Name and Information to Twig
+        return $app['twig']->render('drugs/edit.html.twig', array(
+                'drug_name' => $request->get('ID')
+            ));
+    }
+
 }
