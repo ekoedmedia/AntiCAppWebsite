63011b3cbe92e530b6b18a52596f4e2d526aae86
Zach Lubinski <zachl@shaw.ca>
2014-03-02 17:03:42 -0700

    Started on parsing Drugs Add. All inputs in Drugs named and POST section defined for addAction.

--- src/AntiC/Console/Controller/DrugsController.php
+++ src/AntiC/Console/Controller/DrugsController.php
@@ -12,9 +12,30 @@ class DrugsController
         return $app['twig']->render('drugs/index.html.twig');
     }
 
-    public function addAction(Application $app)
-    {
-        return $app['twig']->render('drugs/add.html.twig');
+    public function addAction(Request $request, Application $app)
+    {        
+        if ($request->getMethod() == 'POST')
+        {
+          /*  $array = array(
+                'g_name' => $request->get('common-name'),
+                't_name' => $request->get('trade-name'),
+                'risk' => $request->get('risk-level'),
+            );
+           */
+            error_log($request->get('g_name'));
+            error_log($request->get('t_name'));
+            error_log($request->get('risk'));
+            foreach($request->get('classification') as $classification){
+                error_log($classification);
+            }
+            
+            return $app['twig']->render('drugs/add.html.twig');
+           # return $array;
+        }
+        else
+        {
+            return $app['twig']->render('drugs/add.html.twig');
+        }
     }
 
     public function editAction(Request $request, Application $app)
