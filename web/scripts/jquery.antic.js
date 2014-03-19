2a0077a7956afd4f9736daab477e78831c0677f2
ekoedmedia <ekoedmedia@gmail.com>
2014-03-19 02:39:03 -0600

    Updated Styles of Drugs Add and Interactions Add pages, made a Smart version of original add button, which now removes input data thats cloned as well as adds remove button if needed

--- web/scripts/jquery.antic.js
+++ web/scripts/jquery.antic.js
@@ -10,4 +10,30 @@ $(function(){
         $.post("/console/user/"+userID+"/enable");
     });
 
+    // Smart Add button, used for Edit and Add pages, based off Zach's original code.
+    $('.addMore').click(function(){
+        var parent = $(this).parent();
+        var clonedRow = parent.find(".row").last().clone();
+
+        // Update the Name
+        clonedRow.find('input:text, input:password, input:file, select, textarea, input:radio, input:checkbox').each(function(){
+            var match = $(this).attr('name').match(/(\d+)/g);
+            if (match) {
+                var number = parseInt(match[0]);
+                number = number + 1;
+                $(this).attr('name', $(this).attr('name').replace(/(\d+)/g, number));
+            }
+        });
+
+        // Clear the inputs
+        clonedRow.find('input:text, input:password, input:file, select, textarea').val('');
+        clonedRow.find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
+
+        // If there is no button, then add the remove, otherwise ignore.
+        if (!clonedRow.find('button').length)
+            clonedRow.append("<div class='col-sm-1'><button type='button' onClick='removeRow(this);' class='btn btn-danger btn-xs remove-input'>\n\
+<span class='glyphicon glyphicon-minus-sign'></span> Remove</button></div>");
+        clonedRow.insertBefore($(this));
+    });
+
 });
